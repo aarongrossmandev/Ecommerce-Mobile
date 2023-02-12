51eeddd5a229868489ccef73d260ef43a08468cf
Aaron <aarongrossman.dev@gmail.com>
2023-02-12 16:12:02 -0500

    productScreen and productDetailScreen basic structure finished

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet,View,} from 'react-native';
+import ProductsScreen from './src/screens/ProductsScreen';
+import ProductDetailsScreen from './src/screens/ProductDetailsScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <ProductsScreen /> */}
+      <ProductDetailsScreen />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +18,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
 });
