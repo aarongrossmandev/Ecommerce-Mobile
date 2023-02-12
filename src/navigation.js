373fa41f35692de616c235899e85e19b905f7847
Aaron <aarongrossman.dev@gmail.com>
2023-02-12 18:33:54 -0500

    navigation screens added

---
+++ src/navigation.js
@@ -0,0 +1,40 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import ProductsScreen from './screens/ProductsScreen';
+import ProductDetailsScreen from './screens/ProductDetailsScreen';
+import ShoppingCart from './screens/ShoppingCart';
+import {FontAwesome5} from '@expo/vector-icons';
+import {Pressable,Text,View} from 'react-native';
+
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{contentStyle: {backgroundColor: 'white'}}}>
+        <Stack.Screen
+          name="Products"
+          component={ProductsScreen}
+          options={({navigation}) => ({
+            headerRight: () => (
+              <Pressable
+                onPress={() => navigation.navigate('Cart')}
+                style={{flexDirection: 'row',position: 'relative'}}
+              >
+                <FontAwesome5 name="shopping-cart" size={18} color="gray" />
+                <View style={{position: 'absolute',top: -12,left: 5,backgroundColor: 'red',width: 15,borderRadius: '100%',justifyContent: 'center',alignItems: 'center'}}>
+                  <Text style={{fontWeight: '500'}}>1</Text>
+                </View>
+              </Pressable>
+            )
+          })}
+        />
+        <Stack.Screen name="Product Details" component={ProductDetailsScreen} options={{presentation: 'modal'}} />
+        <Stack.Screen name="Cart" component={ShoppingCart} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
